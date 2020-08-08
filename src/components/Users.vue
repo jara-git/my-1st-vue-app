c900a26cda65da580f34bc8560b8fa4d80a4eef0
solar <jarabelmonte@gmail.com>
2020-08-08 18:56:24 +0200

    fetch API

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -11,3 +11,22 @@
     margin-bottom: 5%;
   }
 </style>
+
+<script>
+  import axios from 'axios';
+  export default {
+    name: 'Users',
+    data() {
+      return {
+        users: null,
+      };
+    },
+    created: function() {
+      axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+          this.users = res.data;
+        })
+    }
+  }
+</script>
